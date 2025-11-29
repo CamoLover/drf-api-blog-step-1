c0d552b570ea127d6ebecc15e7350c749a724e43
Evan E <118671532+CamoLover@users.noreply.github.com>
2025-11-29 15:13:53 +0100

    added admin

--- blogData/admin.py
+++ blogData/admin.py
@@ -1,7 +1,33 @@
 from django.contrib import admin
 
-from blogData.models import Article, Comment
+from blogData.models import Article, Comment, Concessionnaire, Vehicule
 
 # Register your models here.
 admin.site.register(Article)
 admin.site.register(Comment)
+
+
+@admin.register(Concessionnaire)
+class ConcessionnaireAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nom']  # Masquer siret dans la liste
+    list_filter = ['nom']
+    search_fields = ['nom']
+    fields = ['nom', 'siret']  # Siret reste accessible en édition
+    
+    def get_readonly_fields(self, request, obj=None):
+        # Siret en lecture seule après création pour éviter les erreurs
+        if obj:  # Si c'est une modification
+            return ['siret']
+        return []
+
+
+@admin.register(Vehicule)
+class VehiculeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'marque', 'type', 'chevaux', 'prix_ht', 'concessionnaire']
+    list_filter = ['type', 'concessionnaire', 'marque']
+    search_fields = ['marque', 'concessionnaire__nom']
+    fields = ['type', 'marque', 'chevaux', 'prix_ht', 'concessionnaire']
+    
+    def get_queryset(self, request):
+        # Optimisation pour éviter les requêtes N+1
+        return super().get_queryset(request).select_related('concessionnaire')
