0ca067f429f4617d6d6b6390adfd94fcf5b4a516
Evan E <118671532+CamoLover@users.noreply.github.com>
2025-11-29 14:36:17 +0100

    add model Concessionnaire & Véhicule w/ relations

--- blogData/models.py
+++ blogData/models.py
@@ -1,5 +1,11 @@
-from django.db.models import Model, CharField, TextField, DateTimeField, ForeignKey, CASCADE
+from django.db.models import Model, CharField, TextField, DateTimeField, ForeignKey, CASCADE, FloatField, IntegerField
+from django.core.exceptions import ValidationError
 from apiBlog import settings
+
+
+def validate_siret(value):
+    if len(value) != 14:
+        raise ValidationError('Le SIRET doit faire exactement 14 caractères.')
 
 
 # Create your models here.
@@ -16,3 +22,27 @@ class Comment(Model):
     created_at = DateTimeField(auto_now_add=True)
     updated_at = DateTimeField(auto_now=True)
     article = ForeignKey(Article, on_delete=CASCADE, related_name='comments')
+
+
+class Concessionnaire(Model):
+    nom = CharField(max_length=64)
+    siret = CharField(max_length=14, validators=[validate_siret])
+
+    def __str__(self):
+        return self.nom
+
+
+class Vehicule(Model):
+    TYPE_CHOICES = [
+        ('moto', 'Moto'),
+        ('auto', 'Auto'),
+    ]
+    
+    type = CharField(max_length=4, choices=TYPE_CHOICES)
+    marque = CharField(max_length=64)
+    chevaux = IntegerField()
+    prix_ht = FloatField()
+    concessionnaire = ForeignKey(Concessionnaire, on_delete=CASCADE, related_name='vehicules')
+
+    def __str__(self):
+        return f"{self.marque} ({self.type}) - {self.concessionnaire.nom}"
